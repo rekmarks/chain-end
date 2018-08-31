3d3ece10daa77064af3bb9d83721e452d756e840
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2018-08-31 18:17:33 -0400

    undo dynamic contracts export because fs.readdirSync has permissions problems

--- src/contracts.js
+++ src/contracts.js
@@ -1,29 +1,6 @@
-/**
- * Script for getting Truffle contract artifacts from all contracts 
- * in src/default-contracts
- */
 
-const fs = require('fs')
-const path = require('path')
-
-const contractsPath = __dirname + '/default-contracts'
-
-let contracts = {}
-
-let files = fs.readdirSync(contractsPath)
-
-files.forEach( fileName => {
-
-  split = fileName.split('.')
-
-  if (split.length !== 2) {
-    throw new Error('contracts: invalid file', fileName)
-  }
-  else if (split[1] === 'json') {
-    contracts[split[0]] = require(contractsPath + '/' + fileName)  
-  }
-  else {} // do nothing
-  
-})
-
-module.exports = contracts
+module.exports = {
+  StandardERC20: require('./default-contracts/StandardERC20.json'),
+  MintableERC20: require('./default-contracts/MintableERC20.json'),
+  StandardCrowdsale: require('./default-contracts/StandardCrowdsale.json'),
+}
